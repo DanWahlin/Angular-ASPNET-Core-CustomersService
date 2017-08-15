f8211ead27e6b34a2669df361ce91437e28ace01
Dan Wahlin <danwahlin@Dans-iMac.local>
2017-08-15 15:41:50 -0700

    Including both Http and HttpClient

--- Angular-ASPNET-Core-CustomersService/wwwroot/app/core/core.module.ts
+++ Angular-ASPNET-Core-CustomersService/wwwroot/app/core/core.module.ts
@@ -1,8 +1,6 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
-
-//Can use with Angular 2+ but we're using HttpClientModule (see above) for this app
-//import { HttpModule, XSRFStrategy, CookieXSRFStrategy } from '@angular/http';
+import { HttpModule, Request, XSRFStrategy, CookieXSRFStrategy } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DataService } from './data.service';
 import { DataFilterService } from './data-filter.service';
@@ -11,18 +9,9 @@ import { TrackByService } from './trackby.service';
 import { EnsureModuleLoadedOnceGuard } from '../shared/ensureModuleLoadedOnceGuard';
 
 @NgModule({
-  imports: [ 
-    //Can use with Angular 4.3+ to 
-    HttpClientModule, 
-    //Can use to override default names for XSRF cookie and header
-    // HttpClientXsrfModule.withOptions({
-    //   cookieName: 'My-XSRF-TOKEN',
-    //   headerName: 'My-X-XSRF-TOKEN',
-    // })
-  ],
+  imports: [ HttpModule, HttpClientModule ],
   providers: [
     //Default XSRF provider setup (change cookie or header name if needed): 
-    //Can use this with Angular 2+ but if using HttpClientModule use options as shown above
     //{ provide: XSRFStrategy, useValue: new CookieXSRFStrategy('XSRF-TOKEN', 'X-XSRF-TOKEN') },
     DataService, DataFilterService, Sorter, TrackByService] // these should be singleton
 })
